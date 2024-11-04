42e84c90c8c836529b3977b5313ab67385342457
CaioGGR <caioggr@gmail.com>
2024-11-04 08:28:57 -0300

    incrementando funcoes no gerenciador

--- Modulo1/projeto/gerenciador.py
+++ Modulo1/projeto/gerenciador.py
@@ -1,13 +1,42 @@
 def adicionar_tarefa(tarefas, nome_tarefa):
   tarefa = {"tarefa": nome_tarefa, "completada": False}
   tarefas.append(tarefa)
-  print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
+  print(f"\nTarefa {nome_tarefa} foi adicionada com sucesso!")
+  return
+
+def ver_tarefas(tarefas):
+  print("\nLista de tarefas:")
+  for indice, tarefa in enumerate(tarefas, start=1):
+    status = "✔" if tarefa["completada"] else " "
+    nome_tarefa = tarefa["tarefa"]
+    print(f"{indice}. [{status}] {nome_tarefa}")
+
+def atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
+  indice_tarefa_ajustado = indice_tarefa - 1 # pq na lista que aparece para o usuario, comeca em 1, mas na lista real comeca em 0
+  if indice_tarefa_ajustado >= 0 and indice_tarefa_ajustado < len(tarefas):
+    tarefas[indice_tarefa_ajustado]["tarefa"] = novo_nome_tarefa
+    print(f"Tarefa {indice_tarefa} atualizada para {novo_nome_tarefa}")
+  else:
+    print("A tarefa selecionada nao existe.")
+  return
+
+def completar_tarefa(tarefas, indice_tarefa):
+  indice_tarefa_ajustado = indice_tarefa - 1
+  tarefas[indice_tarefa_ajustado]["completada"] = True
+  print(f"Tarefa {indice_tarefa} marcada como completada.")
+  return
+
+def deletar_tarefas_completadas(tarefas):
+  for tarefa in tarefas:
+    if tarefa["completada"]:
+      tarefas.remove(tarefa)
+  print("Tarefas completadas foram deletadas.")
   return
 
 tarefas = []
 
 while True:
-  print("Menu do Gerenciador de Listas de Tarefas:")
+  print("\nMenu do Gerenciador de Listas de Tarefas:")
   print("1 - Adicionar Tarefa")
   print("2 - Ver tarefas")
   print("3 - Atualizar tarefa")
@@ -20,6 +49,24 @@ while True:
   if escolha == "1":
     nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
     adicionar_tarefa(tarefas, nome_tarefa)
+
+  elif escolha == "2":
+    ver_tarefas(tarefas)
+
+  elif escolha == "3":
+    ver_tarefas(tarefas)
+    indice_tarefa = int(input("Digite o numero de tarefa que deseja atualizar: "))
+    novo_nome = input("Digite o novo nome da tarefa: ")
+    atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome)
+
+  elif escolha == "4":
+    ver_tarefas(tarefas)
+    indice_tarefa = int(input("Digite o numero da tarefa que deseja completar: "))
+    completar_tarefa(tarefas, indice_tarefa)
+
+  elif escolha == "5":
+    deletar_tarefas_completadas(tarefas)
+    ver_tarefas(tarefas)
   elif escolha == "6":
     break
 
